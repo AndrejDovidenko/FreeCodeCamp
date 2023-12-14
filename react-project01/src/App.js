e5289ec7434d06228901a0cb8e870e0b76a1008a
AndrejDovidenko <andrejdovidenko@gmail.com>
2023-12-14 19:20:24 +0300

    react lesson05

--- react-project01/src/App.js
+++ react-project01/src/App.js
@@ -1,15 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 const JSX = (
-  <div id="challenge-node">
-    <h1>Hello World</h1>
-    <p>Lets render this to the DOM</p>
+  <div className="myDiv">
+    <h1>Add a class to this div</h1>
   </div>
 );
-// Add your code below this line
 
-const div = document.getElementById("challenge-node");
-ReactDOM.render(JSX, div);
 function App() {
   return JSX;
 }
