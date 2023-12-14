fe2370cb9e0e01f5099b306a0a04c0742b0c0477
AndrejDovidenko <andrejdovidenko@gmail.com>
2023-12-14 19:24:25 +0300

    react lesson06

--- react-project01/src/App.js
+++ react-project01/src/App.js
@@ -1,8 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
 const JSX = (
-  <div className="myDiv">
-    <h1>Add a class to this div</h1>
+  <div>
+    <h2>Welcome to React!</h2> <br />
+    <p>Be sure to close all tags!</p>
+    <hr />
   </div>
 );
 
